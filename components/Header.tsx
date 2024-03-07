b3653fcc8a5ada5c5a8a07086aa1b11e7c9e2ca3
JethroNatividad <natividad.jet@gmail.com>
2024-03-07 23:53:44 +0800

    Style Menu

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="px-5 py-5 border-b border-slate-600 fixed top-0 left-0 w-full z-20 bg-black">
+      <h1 className="text-3xl font-bold">Clicking Crazy</h1>
+    </header>
+  );
+};
+
+export default Header;
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,8 @@
 import { useGame } from "@/store/game";
 import React from "react";
+import HeroImg from "@/assets/images/hero.png";
+import Image from "next/image";
+import Header from "./Header";
 
 const Menu = () => {
   const { setGameState } = useGame();
@@ -9,16 +12,25 @@ const Menu = () => {
   };
 
   return (
-    <main className="h-screen w-full flex items-center justify-center">
-      <div className="flex flex-col items-center space-y-5">
-        <h1 className="text-3xl font-bold">Clicking Race</h1>
-        <button
-          className="px-5 py-3 border border-black rounded-md"
-          onClick={handlePlayGame}
-        >
-          Play Game
-        </button>
-      </div>
+    <main className="h-screen w-full flex items-center justify-center relative text-slate-300">
+      <Header />
+      <section className="relative h-screen w-full flex items-center justify-center">
+        <Image
+          className="absolute top-0 left-0 h-full object-cover w-full"
+          src={HeroImg}
+          alt="hero"
+        />
+        <div className="z-10 text-center space-y-5 px-5">
+          <h1 className="text-4xl">The Craziest Clicking Race Game</h1>
+          <p>Destroy your opponents by clicking faster than them.</p>
+          <button
+            onClick={handlePlayGame}
+            className="px-5 py-3 border-slate-600 border rounded-md text-xl font-bold bg-slate-800 hover:bg-slate-700"
+          >
+            Play Game
+          </button>
+        </div>
+      </section>
     </main>
   );
 };
