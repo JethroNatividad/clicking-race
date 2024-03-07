b283fe3668019b84799dad6a5c8b4ad032121767
JethroNatividad <natividad.jet@gmail.com>
2024-03-08 00:27:17 +0800

    Style menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,7 @@
 import { useGame } from "@/store/game";
 import React from "react";
+import Background from "@/assets/images/bg.png";
+import Image from "next/image";
 
 const Menu = () => {
   const { setGameState } = useGame();
@@ -9,16 +11,21 @@ const Menu = () => {
   };
 
   return (
-    <main className="h-screen w-full flex items-center justify-center">
-      <div className="flex flex-col items-center space-y-5">
-        <h1 className="text-3xl font-bold">Clicking Race</h1>
+    <main className="h-screen w-full flex items-center justify-center relative">
+      <div className="flex flex-col items-center space-y-10 z-10">
+        <h1 className="text-7xl font-bold">Clicking Race</h1>
         <button
-          className="px-5 py-3 border border-black rounded-md"
+          className="px-5 py-3 border text-xl border-black rounded-md hover:bg-black hover:text-white transition-all"
           onClick={handlePlayGame}
         >
           Play Game
         </button>
       </div>
+      <Image
+        src={Background}
+        alt="background"
+        className="absolute top-0 left-0 h-full w-full object-cover"
+      />
     </main>
   );
 };
