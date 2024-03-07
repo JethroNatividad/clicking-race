24da0f496dc5da1604372e1331f7fb15411c5b97
JethroNatividad <natividad.jet@gmail.com>
2024-03-07 18:43:30 +0800

    Add players

--- components/Configure.tsx
+++ components/Configure.tsx
@@ -1,15 +1,15 @@
+import { useGame } from "@/store/game";
 import React from "react";
+import ConfigureCar from "./ConfigureCar";
 
 const Configure = () => {
+  const { players } = useGame();
   return (
     <main className="p-10">
       <div className="grid grid-cols-6 h-40 gap-10">
-        <div className="bg-black h-full w-full"></div>
-        <div className="bg-black h-full w-full"></div>
-        <div className="bg-black h-full w-full"></div>
-        <div className="bg-black h-full w-full"></div>
-        <div className="bg-black h-full w-full"></div>
-        <div className="bg-black h-full w-full"></div>
+        {players.map((player) => (
+          <ConfigureCar key={player.id} player={player} />
+        ))}
       </div>
       <div>
         {/* Bind */}
